278f8988eb7a4a7003ac864351f2341fa3d32776
norberto-vx <norberto.silva@vx.med.br>
2024-02-07 14:46:19 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    parserOptions: {
+        project: '../../tsconfig.json',
+        tsconfigRootDir: __dirname,
+    },
+    parser: '@typescript-eslint/parser',
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    env: {
+        'node': true,
+    },
+    plugins: [
+        '@typescript-eslint/eslint-plugin',
+        'react'
+    ],
+    extends: [
+        'eslint:recommended',
+     
+    ],
+
+    rules: {
+
+        // Permite apenas console info 
+        'no-console': ['warn', { 'allow': ['info'] }],
+
+    }
+}
