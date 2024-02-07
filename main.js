0e5be40f336e107311c562e4871e72646e4ffebf
norberto-vx <137805433+norberto-vx@users.noreply.github.com>
2024-02-07 15:58:59 -0300

    add

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ module.exports = {
     ],
     extends: [
         'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended'
+        'plugin:@typescript-eslint/recommended',
     ],
 
     rules: {
