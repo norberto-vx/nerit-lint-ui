fe97d7b0baaa4cc74d2da0163a768a6b22df82f3
norberto-vx <137805433+norberto-vx@users.noreply.github.com>
2024-02-07 21:45:52 -0300

    add

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 module.exports = {
+    root: true,
+    parserOptions: {
+        project: '../../tsconfig.json',
+        tsconfigRootDir: __dirname,
+    },
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint',
+        'eslint-plugin-import-helpers'      
+    ],
     extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended' ,
         'nerit-lint-api'     
     ]
 }
